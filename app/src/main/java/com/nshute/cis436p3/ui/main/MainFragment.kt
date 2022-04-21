032ab6e10e02bbfc7d14110c76fe391770f89d92
tartilc <jampackt45@gmail.com>
2022-04-21 02:54:06 -0400

    changed it up

--- app/src/main/java/com/nshute/cis436p3/ui/main/MainFragment.kt
+++ app/src/main/java/com/nshute/cis436p3/ui/main/MainFragment.kt
@@ -3,50 +3,26 @@ package com.nshute.cis436p3.ui.main
 import android.content.Context
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.android.volley.Request
-import com.android.volley.Response
+import com.nshute.cis436p3.R
+import com.nshute.cis436p3.databinding.MainFragmentBinding
+import android.widget.ImageView
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
-import com.nshute.cis436p3.R
+import retrofit2.Call
+import retrofit2.Response
 import org.json.JSONArray
+import com.bumptech.glide.Glide
 
 class MainFragment : Fragment() {
-    val url = "https://api.thecatapi.com/v1/breeds" //for dropdown population
-    //val search_url = "https://api.thecatapi.com/images/search?breed_id=${selected_breed.id}" //for dropdown population
+    lateinit var  binding : MainFragmentBinding
 
-    private val breedName = arrayOf<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //spinner=(Spinner)findViewById(R.id.)
-        //get_json()
-        //pass somethiing to fragment
     }
-
-    fun get_json(){
-        val queue = Volley.newRequestQueue(context)
-        val request = StringRequest(
-            Request.Method.GET, url,
-            { response ->
-                val data = response.toString()
-                var jArray = JSONArray(data)
-                for(i in 0..jArray.length()-1){
-                    var jobject = jArray.getJSONObject(i)
-                    var name = jobject.getInt("name")
-                    var description = jobject.getInt("description")
-                    var origin = jobject.getString("origin")
-                    var lifespan = jobject.getString("lifespan")
-                    var temp = jobject.getString("temperament")
-                }
-            },
-            Response.ErrorListener {  })
-        queue.add((request))
-    }
-
 
     companion object {
         fun newInstance() = MainFragment()
@@ -58,6 +34,15 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        binding = MainFragmentBinding.inflate(layoutInflater)
+
+        val resizeImage = "https://cdn2.thecatapi.com/images/oc.jpg"
+
+        Glide.with(this)
+            .load(resizeImage)
+            .into(binding.catPic)
+
+
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
