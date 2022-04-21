ae3291918bdf5ca2c81a2180573f994b9bdfd608
tgreer4 <95147728+tgreer4@users.noreply.github.com>
2022-04-20 20:45:17 -0400

    added get_json

--- app/src/main/java/com/nshute/cis436p3/ui/main/MainFragment.kt
+++ app/src/main/java/com/nshute/cis436p3/ui/main/MainFragment.kt
@@ -2,17 +2,52 @@ package com.nshute.cis436p3.ui.main
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.nshute.cis436p3.R
+import org.json.JSONArray
 
-class MainFragment : Fragment() {
+abstract class MainFragment : Fragment() {
+    val url = "https://api.thecatapi.com/v1/breeds" //for dropdown population
+    //val search_url = "https://api.thecatapi.com/images/search?breed_id=${selected_breed.id}" //for dropdown population
 
-    companion object {
-        fun newInstance() = MainFragment()
+    private val breedName = arrayOf<String>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //spinner=(Spinner)findViewById(R.id.)
+        get_json()
     }
+
+    fun get_json(){
+        val queue = Volley.newRequestQueue(this)
+        val request = StringRequest(
+            Request.Method.GET, url,
+            { response ->
+                val data = response.toString()
+                var jArray = JSONArray(data)
+                for(i in 0..jArray.length()-1){
+                    var jobject = jArray.getJSONObject(i)
+                    var userId = jobject.getInt("userId")
+                    var id = jobject.getInt("id")
+                    var title = jobject.getString("title")
+                    var body = jobject.getString("body")
+                }
+                Log.e("Error", response.toString())
+            },
+            Response.ErrorListener {  })
+        queue.add((request))
+    }
+
+    //companion object {
+        //fun newInstance() = MainFragment()
+    //}
 
     private lateinit var viewModel: MainViewModel
 
--- app/src/main/java/com/nshute/cis436p3/ui/main/SpinnerViewModel.kt
+++ app/src/main/java/com/nshute/cis436p3/ui/main/SpinnerViewModel.kt
@@ -3,5 +3,6 @@ package com.nshute.cis436p3.ui.main
 import androidx.lifecycle.ViewModel
 
 class SpinnerViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+
 }
